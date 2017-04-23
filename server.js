0262508a7158d5272673776192e12bc9e9f27278
Ivan Villa <trezeguet55@gmail.com>
2017-04-22 21:12:29 -0500

    <server.js> refactor imports of ParamedicRoutes and EmergencyRoutes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
 
 const {AmbulanceRoutes} = require('./api/ambulance/routes');
-const routerParamedic = require('./api/paramedic/routes');
-const routerEmergency = require('./api/emergency/routes');
+const {ParamedicRoutes} = require('./api/paramedic/routes');
+const {EmergencyRoutes} = require('./api/emergency/routes');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -16,7 +16,7 @@ app.use(bodyparser.json());
 
 
 let ambulanceRouter = new AmbulanceRoutes(app);
-app.use('/api', routerParamedic);
-app.use('/api', routerEmergency);
+let paramedicRoutes = new ParamedicRoutes(app);
+let emergencyRoutes = new EmergencyRoutes(app);
 
 const server = app.listen(port, () => console.log(`server listening in port ${port}`));
