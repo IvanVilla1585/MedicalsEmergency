4ac1d31839a01b8e1c7ca13b817de86d6585ee7f
Ivan Villa <trezeguet55@gmail.com>
2017-07-01 11:06:40 -0500

    <user model.js> the permissions attribute  are created

--- api/oauth2/user/model.js
+++ api/oauth2/user/model.js
@@ -11,6 +11,10 @@ var UserSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    permissions: {
+        type: [String],
+      required: true
     }
 });
 
