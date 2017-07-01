2f59f58b7d46cd2fa8dab7e19d9cc0c9786aab1a
Manuela <manuelao@s4ds.com>
2017-06-30 21:23:21 -0500

    Se agregan los archivos y se configura toda la parte de mensajes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const {AmbulanceRoutes} = require('./api/ambulance/routes');
 const {ParamedicRoutes} = require('./api/paramedic/routes');
 const {EmergencyRoutes} = require('./api/emergency/routes');
+const {Receiver} = require('./amqp/receiver');
+const {Sender} = require('./amqp/sender');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -18,5 +20,12 @@ app.use(bodyparser.json());
 let ambulanceRouter = new AmbulanceRoutes(app);
 let paramedicRoutes = new ParamedicRoutes(app);
 let emergencyRoutes = new EmergencyRoutes(app);
+
+//var sender = new Sender('amqp://ivan:ivan@10.100.75.131','manuelao');
+//sender.sendMessage('Hola Ivan');
+var receiver = new Receiver('amqp://ivan:ivan@10.100.75.131','manuelao');
+receiver.receiveMessage(function(msg){
+	console.log('Mensaje recibido' + msg);
+});
 
 const server = app.listen(port, () => console.log(`server listening in port ${port}`));
