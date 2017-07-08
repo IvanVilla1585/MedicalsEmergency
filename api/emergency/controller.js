b8ed9aa46dc94ee71540412febdbd089989d3ad4
Manuela <manuelao@s4ds.com>
2017-07-07 21:29:23 -0500

    Se eliminan los archivos receiver y sender. Se agrega la logica para el envio de mensajes al momento de guardar la emergencia

--- api/emergency/controller.js
+++ api/emergency/controller.js
@@ -1,4 +1,7 @@
 var { EmergencyModel } = require('./modelschema');
+var { Client } = require('../../amqp/request_reply');
+
+let client = new Client('amqp://hospital:hospital1@10.100.127.28','manuelao');
 
 class EmergencyController {
   loadEmergencyById(req, res, next, id) {
@@ -21,9 +24,17 @@ class EmergencyController {
       res.status(500);
       return res.json({message: 'Ingrese los campos obligatorios'});
     }
-    emergency.save()
-      .then(savedEmergency => res.json(savedEmergency))
+    client.sendRequest(JSON.stringify(emergency), function(reply){
+        console.log(`Respuesta recibida con exito ${reply}`);
+        let replyHospitales = JSON.parse(reply);
+        emergency.hospital = replyHospitales.hospitales;
+    });
+    setTimeout(function(){ 
+      emergency.save()
+      .then(savedEmergency =>  res.json(savedEmergency))
       .catch(e => next(e));
+    }, 10000);
+    
   }
 
   listEmergencies(query, res, next) {
